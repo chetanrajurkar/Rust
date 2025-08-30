404d42e31bb504044c89279bbfd9af29353fabc3
chetan07 <chetanrajurkar38@gmail.com>
2025-08-30 23:58:51 +0530

    first Program with cargo

---
+++ Day_1/target/debug/Day_1.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/91951/Desktop/Rust/Day_1/target/debug/Day_1: /mnt/c/Users/91951/Desktop/Rust/Day_1/src/main.rs
---
+++ Day_1/target/debug/deps/Day_1-d7220f8e881eb534.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/91951/Desktop/Rust/Day_1/target/debug/deps/Day_1-d7220f8e881eb534.d: src/main.rs
+
+/mnt/c/Users/91951/Desktop/Rust/Day_1/target/debug/deps/Day_1-d7220f8e881eb534: src/main.rs
+
+src/main.rs:
