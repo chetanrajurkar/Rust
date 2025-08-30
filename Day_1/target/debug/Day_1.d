ffebf184169063b6b279277d8d68651056310319
chetan07 <chetanrajurkar38@gmail.com>
2025-08-31 01:12:43 +0530

    moved to saparate file

--- Day_1/target/debug/Day_1.d
+++
@@ -1,1 +0,0 @@
-/mnt/c/Users/91951/Desktop/Rust/Day_1/target/debug/Day_1: /mnt/c/Users/91951/Desktop/Rust/Day_1/src/main.rs
--- Day_1/target/debug/deps/Day_1-d7220f8e881eb534.d
+++
@@ -1,5 +0,0 @@
-/mnt/c/Users/91951/Desktop/Rust/Day_1/target/debug/deps/Day_1-d7220f8e881eb534.d: src/main.rs
-
-/mnt/c/Users/91951/Desktop/Rust/Day_1/target/debug/deps/Day_1-d7220f8e881eb534: src/main.rs
-
-src/main.rs:
